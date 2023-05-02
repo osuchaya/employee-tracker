a3c3b4c3ee8619874115d5a58a636b7fb1884c23
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-05-01 21:02:05 -0700

    Added View All departments and view all roles functions

--- db.js
+++ db.js
@@ -33,12 +33,37 @@ class Database {
             return managers;
         });
     }
+    getEmployees() {
+        return this.getManagers();
+    }
+    getDepartments() {
+        return this.db.promise().query( `SELECT name FROM department`).then(result => {
+            const data = result[0];
+            let departments = [];
+            for (let i=0; i<data.length; i++) {
+                departments.push(data[i]['name']);
+            }
+            return departments;
+        })
+    }
 
     insertEmployee(firstName, lastName, roleID, managerID) {
         let query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
         return this.db.promise().query(query, [firstName, lastName, roleID, managerID])
     }
+    insertDepartment(name) {
+        let query = `INSERT INTO department (name) VALUE (?)`;
+        return this.db.promise().query(query, [name])
+    }
+    insertRole(role, salary, department_id) {
+        let query = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+        return this.db.promise().query(query, [role, salary, department_id])
+    }
+    updateEmployeeRole(employee_id, role_id) {
+        let query = `UPDATE employee SET role_id=? WHERE employee.id=?`;
+        return this.db.promise().query(query, [role_id, employee_id])
 
+    }
     viewEmployees() {
         let query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary,
          CONCAT(manager.first_name," ",manager.last_name) AS manager FROM employee 
@@ -56,6 +81,13 @@ class Database {
             return result[0];
         })
     }
+    viewRoles() {
+        let query = `SELECT role.id, title, salary, department.name AS department FROM role INNER JOIN department ON role.department_id=department.id`;
+        return this.db.promise().query(query).then(result => {
+            return result[0];
+        })
+    }
+
  }
 
 module.exports = Database
--- index.js
+++ index.js
@@ -1,116 +1,170 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 //Import mysql2
 
 //Import console.table for adding data into table
-const cTable = require('console.table');
-const Database = require('./db.js');
-
-
-
+const cTable = require("console.table");
+const Database = require("./db.js");
 
 //Connect to database
-const db = new Database()
+const db = new Database();
 
-
-const question =
-{
-    type: 'list',
-    message: 'What would you like to do?',
-    choices: ['View All Employees', 'View All Departments','Add Employee', 'Quit'],
-    name: 'Action',
-}
+const question = {
+  type: "list",
+  message: "What would you like to do?",
+  choices: [
+    "View All Employees",
+    "View All Departments",
+    "View All Roles",
+    "Update Employee Role",
+    "Add Employee",
+    "Add Department",
+    "Add A Role",
+    "Quit",
+  ],
+  name: "Action",
+};
 let quit = false;
 function converttoChoices(names) {
-    let choices = [];
-    for (let i = 0; i < names.length; i++) {
-        let choice = {
-            name: names[i],
-            value: i + 1
-        }
-        choices.push(choice);
-    }
+  let choices = [];
+  for (let i = 0; i < names.length; i++) {
+    let choice = {
+      name: names[i],
+      value: i + 1,
+    };
+    choices.push(choice);
+  }
 
-    return choices;
+  return choices;
 }
 
 function getAddEmployeeQuestions() {
-    return db.getRoles().then(roles => {
-
-        return db.getManagers().then(managers => {
-
-
-            return [
-                {
-                    type: 'input',
-                    message: "What is employee's first name?",
-                    name: 'firstName'
-                },
-                {
-                    type: 'input',
-                    message: "What is employee's last name?",
-                    name: 'lastName'
-                },
-                {
-                    type: 'list',
-                    message: "What is employee's role?",
-                    name: 'roleID',
-                    choices: converttoChoices(roles)
-                },
-                {
-                    type: 'list',
-                    message: "Who is the employee's manager?",
-                    name: 'managerID',
-                    choices: converttoChoices(managers)
-                }
-            ]
-        })
-    }
-    )
+  return db.getRoles().then((roles) => {
+    return db.getManagers().then((managers) => {
+      return [
+        {
+          type: "input",
+          message: "What is employee's first name?",
+          name: "firstName",
+        },
+        {
+          type: "input",
+          message: "What is employee's last name?",
+          name: "lastName",
+        },
+        {
+          type: "list",
+          message: "What is employee's role?",
+          name: "roleID",
+          choices: converttoChoices(roles),
+        },
+        {
+          type: "list",
+          message: "Who is the employee's manager?",
+          name: "managerID",
+          choices: converttoChoices(managers),
+        },
+      ];
+    });
+  });
 }
-function addEmployee() {
-    // return inquirer.prompt(getAddEmployeeQuestions())
-    return getAddEmployeeQuestions().then(questions => {
-        return inquirer.prompt(questions)
-    })
+
+const departmentQuestion = {
+  type: "input",
+  message: "What is the name of the department?",
+  name: "name",
 };
-
+function getRoleQuestions() {
+  return db.getDepartments().then((departments) => {
+    return [
+      {
+        type: "input",
+        message: "What is the name of the role?",
+        name: "role",
+      },
+      {
+        type: "number",
+        message: "What is the salary for the role?",
+        name: "salary",
+      },
+      {
+        type: "list",
+        message: "What is the department the role is in?",
+        choices: converttoChoices(departments),
+        name: "department_id",
+      },
+    ];
+  });
+}
+function getUpdateEmployeeQuestions() {
+  return db.getEmployees().then((employees) => {
+    return db.getRoles().then((roles) => {
+      return [
+        {
+          type: "list",
+          message: "Which employee's role do you want to update?",
+          choices: converttoChoices(employees),
+        },
+        {
+            type: "list",
+            message: ""
+        }
+      ];
+    });
+  });
+}
 function promptQuestion() {
-    return inquirer.prompt(question).then(({ Action }) => {
-        if (Action === 'Add Employee') {
-
-            return addEmployee().then(({ firstName, lastName, roleID, managerID }) => {
-                db.insertEmployee(firstName, lastName, roleID, managerID)
-                    .then(result => {
-                        return promptQuestion()
-                    })
-            })
-
-        }
-        else if (Action === 'View All Employees') {
-            return db.viewEmployees().then((result => {
-                console.table(result);
-                return promptQuestion();
-            }));
-
-        }
-        else if (Action === 'View All Departments') {
-            return db.viewDepartment().then((result) => {
-                console.table(result);
-                return promptQuestion();
-            })
-        }
-        else if (Action !== 'Quit') {
+  return inquirer.prompt(question).then(({ Action }) => {
+    if (Action === "Add Employee") {
+      return getAddEmployeeQuestions()
+        .then((questions) => {
+          return inquirer.prompt(questions);
+        })
+        .then(({ firstName, lastName, roleID, managerID }) => {
+          db.insertEmployee(firstName, lastName, roleID, managerID).then(
+            (result) => {
+              return promptQuestion();
+            }
+          );
+        });
+    } else if (Action === "Add Department") {
+      return inquirer.prompt(departmentQuestion).then(({ name }) => {
+        db.insertDepartment(name).then((result) => {
+          return promptQuestion();
+        });
+      });
+    } else if (Action === "Add A Role") {
+      return getRoleQuestions()
+        .then((questions) => {
+          return inquirer.prompt(questions);
+        })
+        .then(({ role, salary, department_id }) => {
+          db.insertRole(role, salary, department_id).then((result) => {
             return promptQuestion();
-        }
-      
-        return Action;
-    })
+          });
+        });
+    } else if (Action === "View All Employees") {
+      return db.viewEmployees().then((result) => {
+        console.table(result);
+        return promptQuestion();
+      });
+    } else if (Action === "View All Departments") {
+      return db.viewDepartment().then((result) => {
+        console.table(result);
+        return promptQuestion();
+      });
+    } else if (Action === "View All Roles") {
+      return db.viewRoles().then((result) => {
+        console.table(result);
+        return promptQuestion();
+      });
+    } else if (Action !== "Quit") {
+      return promptQuestion();
+    }
+
+    return Action;
+  });
 }
 promptQuestion();
-
-
-
-
 
 // db.query(`SELECT * FROM employee`, (err, result) => {
 //     if (err) {
@@ -118,4 +172,3 @@ promptQuestion();
 //     }
 //     console.log(result);
 // });
-
